38c29a63b9f4f94a6f53b781ebcafacf20fef0e6
Marcus Britanicus <marcusbritanicus@gmail.com>
2022-11-10 15:10:32 +0530

    Take plugin name as --config-backend argument
    Instead of providing the full path, provide plugin name as the option's argument

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <signal.h>
 #include <map>
 #include <fcntl.h>
+#include <filesystem>
 
 #include <unistd.h>
 #include <wayfire/debug.hpp>
@@ -153,7 +154,40 @@ static std::optional<std::string> choose_socket(wl_display *display)
 
 static wf::config_backend_t *load_backend(const std::string& backend)
 {
-    auto [_, init_ptr] = wf::get_new_instance_handle(backend);
+    std::string config_plugin(backend);
+
+    if (backend.size())
+    {
+        /** Get the full path if the given name is not a full path */
+        if (backend.compare(0, 1, "/") != 0)
+        {
+            // Not a full path so try to get the full path.
+            std::vector<std::string> plugin_prefixes;
+            if (char *plugin_path = getenv("WAYFIRE_PLUGIN_PATH"))
+            {
+                std::stringstream ss(plugin_path);
+                std::string entry;
+                while (std::getline(ss, entry, ':'))
+                {
+                    plugin_prefixes.push_back(entry);
+                }
+            }
+
+            plugin_prefixes.push_back(PLUGIN_PATH);
+
+            for (std::filesystem::path plugin_prefix : plugin_prefixes)
+            {
+                auto plugin_path = plugin_prefix / ("lib" + backend + ".so");
+                if (std::filesystem::exists(plugin_path))
+                {
+                    config_plugin = plugin_path;
+                    break;
+                }
+            }
+        }
+    }
+
+    auto [_, init_ptr] = wf::get_new_instance_handle(config_plugin);
 
     if (!init_ptr)
     {
