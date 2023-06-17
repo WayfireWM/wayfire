WayfireWM/wayfire

commit bf1c881e49ddb8c45f5fb81c42ed14599a0ab3f6

core: handle wlroots grab end for keyboard and pointer (#1803)

src/core/seat/seat.cpp
@@ -17,6 +17,8 @@
 #include <wayfire/nonstd/wlroots.hpp>
 #include <wayfire/seat.hpp>
 #include <string>
+#include "../../view/view-keyboard-interaction.hpp"
+#include "../../view/wlr-surface-pointer-interaction.hpp"
 
 
 #include "drag-icon.hpp"
@@ -102,20 +104,39 @@ wf::seat_t::seat_t(wl_display *display, std::string name) : seat(wlr_seat_create
     priv->request_set_selection.set_callback([&] (void *data)
     {
         auto ev = static_cast<wlr_seat_request_set_selection_event*>(data);
-        wlr_seat_set_selection(wf::get_core().get_current_seat(),
-            ev->source, ev->serial);
+        wlr_seat_set_selection(wf::get_core().get_current_seat(), ev->source, ev->serial);
     });
     priv->request_set_selection.connect(&seat->events.request_set_selection);
 
     priv->request_set_primary_selection.set_callback([&] (void *data)
     {
-        auto ev =
-            static_cast<wlr_seat_request_set_primary_selection_event*>(data);
-        wlr_seat_set_primary_selection(wf::get_core().get_current_seat(),
-            ev->source, ev->serial);
+        auto ev = static_cast<wlr_seat_request_set_primary_selection_event*>(data);
+        wlr_seat_set_primary_selection(wf::get_core().get_current_seat(), ev->source, ev->serial);
     });
-    priv->request_set_primary_selection.connect(
-        &seat->events.request_set_primary_selection);
+    priv->request_set_primary_selection.connect(&seat->events.request_set_primary_selection);
+
+    priv->on_wlr_keyboard_grab_end.set_callback([&] (void*)
+    {
+        if (priv->keyboard_focus &&
+            dynamic_cast<view_keyboard_interaction_t*>(&priv->keyboard_focus->keyboard_interaction()))
+        {
+            priv->keyboard_focus->keyboard_interaction().handle_keyboard_enter(this);
+        }
+    });
+    priv->on_wlr_keyboard_grab_end.connect(&seat->events.keyboard_grab_end);
+
+    priv->on_wlr_pointer_grab_end.set_callback([&] (void*)
+    {
+        if (auto focus = priv->lpointer->get_focus())
+        {
+            if (dynamic_cast<wlr_surface_pointer_interaction_t*>(&focus->pointer_interaction()))
+            {
+                wf::pointf_t local = get_node_local_coords(focus.get(), priv->cursor->get_cursor_position());
+                focus->pointer_interaction().handle_pointer_enter(local);
+            }
+        }
+    });
+    priv->on_wlr_pointer_grab_end.connect(&seat->events.pointer_grab_end);
 
     priv->on_new_device = [&] (wf::input_device_added_signal *ev)
     {

src/core/seat/seat-impl.hpp
@@ -9,6 +9,7 @@
 #include "wayfire/output.hpp"
 #include "wayfire/input-device.hpp"
 #include "wayfire/scene-input.hpp"
+#include "wayfire/util.hpp"
 
 namespace wf
 {
@@ -95,6 +96,9 @@ struct seat_t::impl
     void update_capabilities();
 
     void force_release_keys();
+
+    wf::wl_listener_wrapper on_wlr_keyboard_grab_end;
+    wf::wl_listener_wrapper on_wlr_pointer_grab_end;
 };
 }
 

src/view/wlr-surface-node.cpp
@@ -5,7 +5,7 @@
 #include "wayfire/render-manager.hpp"
 #include "wayfire/scene-render.hpp"
 #include "wayfire/scene.hpp"
-#include "wlr-surface-pointer-interaction.cpp"
+#include "wlr-surface-pointer-interaction.hpp"
 #include "wlr-surface-touch-interaction.cpp"
 #include <memory>
 #include <sstream>

src/view/xdg-shell.cpp
@@ -36,17 +36,6 @@ wayfire_xdg_popup::wayfire_xdg_popup(wlr_xdg_popup *popup) :
             if (ev->new_focus != popup_parent->get_surface_root_node())
             {
                 this->close();
-
-                // FIXME: hack to get focus working immediately after the popup is closed. The problem is that
-                // the focus was given over to the node, but wlroots has an underlying grab for popups, so the
-                // new surface has not received focus yet.
-                // When the popup is closed, we need to re-enter the new node, however, we can only safely do
-                // that for wlroots surfaces. Other nodes are not affected by wlroots grabs anyway.
-                if (ev->new_focus &&
-                    dynamic_cast<view_keyboard_interaction_t*>(&ev->new_focus->keyboard_interaction()))
-                {
-                    ev->new_focus->keyboard_interaction().handle_keyboard_enter(wf::get_core().seat.get());
-                }
             }
         };
 

commit 2fd6a68883ce8374bf9b27135ae80be3a220f25b

seat: ignore drag grab reset
This fixes a bug introduced by #1803, where dropping causes
wl_pointer.enter to be sent before the drag has ended, resulting in
weird behavior in clients.

src/core/seat/seat.cpp
@@ -127,6 +127,12 @@ wf::seat_t::seat_t(wl_display *display, std::string name) : seat(wlr_seat_create
 
     priv->on_wlr_pointer_grab_end.set_callback([&] (void*)
     {
+        if (priv->drag_active)
+        {
+            // Drag is handled separately.
+            return;
+        }
+
         if (auto focus = priv->lpointer->get_focus())
         {
             if (dynamic_cast<wlr_surface_pointer_interaction_t*>(&focus->pointer_interaction()))