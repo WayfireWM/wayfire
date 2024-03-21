WayfireWM/wayfire

commit 3d2ddcaa1962ece5cccb5979c7f4c1992682284a

output-layout: select mode with closest matching refresh rate (#2229)
This makes it possible to avoid the annoying cases where the monitor
provides 59994 as a refresh rate, and the user cannot say to use @60
instead.

src/core/output-layout.cpp
@@ -67,7 +67,9 @@ wlr_output_mode *find_matching_mode(wlr_output *output,
                 return mode;
             }
 
-            if (!best || (best->refresh < mode->refresh))
+            const int bestSoFar = std::abs(best->refresh - reference.refresh);
+            const int current   = std::abs(mode->refresh - reference.refresh);
+            if (!best || (bestSoFar > current))
             {
                 best = mode;
             }

commit 3a20e5d676033685a9e90675b242e4c738a32f24

output-layout: fix null pointer caused by #2229

src/core/output-layout.cpp
@@ -10,6 +10,7 @@
 #include "../output/output-impl.hpp"
 #include <xf86drmMode.h>
 #include <cstring>
+#include <climits>
 #include <unordered_set>
 #include <drm_fourcc.h>
 #include <wayfire/seat.hpp>
@@ -67,7 +68,7 @@ wlr_output_mode *find_matching_mode(wlr_output *output,
                 return mode;
             }
 
-            const int bestSoFar = std::abs(best->refresh - reference.refresh);
+            const int bestSoFar = best ? std::abs(best->refresh - reference.refresh) : INT_MAX;
             const int current   = std::abs(mode->refresh - reference.refresh);
             if (!best || (bestSoFar > current))
             {