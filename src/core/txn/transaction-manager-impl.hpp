WayfireWM/wayfire

commit 5110f63388e5cffa54d0ae4cd5b2762c7e584cf8

toplevels: keep self reference as long as there are any pending tx

src/view/xdg-shell/xdg-toplevel-view.cpp
@@ -323,7 +323,8 @@ void wf::xdg_toplevel_view_t::handle_toplevel_state_changed(wf::toplevel_state_t
     wf::view_implementation::emit_toplevel_state_change_signals({this}, old_state);
     scene::update(this->get_surface_root_node(), scene::update_flag::GEOMETRY);
 
-    if (!wtoplevel->current().mapped)
+    if (!wf::get_core().tx_manager->is_object_committed(wtoplevel) &&
+        !wf::get_core().tx_manager->is_object_pending(wtoplevel))
     {
         // Drop self-ref => `this` might get deleted
         _self_ref.reset();

src/view/xwayland/xwayland-toplevel-view.hpp
@@ -463,7 +463,8 @@ class wayfire_xwayland_view : public wf::toplevel_view_interface_t, public wayfi
         wf::view_implementation::emit_toplevel_state_change_signals({this}, old_state);
         wf::scene::update(this->get_surface_root_node(), wf::scene::update_flag::GEOMETRY);
 
-        if (!toplevel->current().mapped)
+        if (!wf::get_core().tx_manager->is_object_committed(toplevel) &&
+            !wf::get_core().tx_manager->is_object_pending(toplevel))
         {
             // Drop self-reference => object might be deleted afterwards
             _self_ref.reset();

commit 793a5eba3843d7ea3555a068531998b1410ade4e

toplevel-views: fix memory leak
Introduced in #2312, we need to check for pending transactions, because
while applying, the toplevel is still marked as committed.

src/core/txn/transaction-manager-impl.hpp
@@ -1,7 +1,6 @@
 #include "wayfire/signal-provider.hpp"
 #include "wayfire/txn/transaction.hpp"
 #include <algorithm>
-#include <iterator>
 #include <wayfire/txn/transaction-manager.hpp>
 #include <wayfire/debug.hpp>
 

src/view/xdg-shell/xdg-toplevel-view.cpp
@@ -323,8 +323,7 @@ void wf::xdg_toplevel_view_t::handle_toplevel_state_changed(wf::toplevel_state_t
     wf::view_implementation::emit_toplevel_state_change_signals({this}, old_state);
     scene::update(this->get_surface_root_node(), scene::update_flag::GEOMETRY);
 
-    if (!wf::get_core().tx_manager->is_object_committed(wtoplevel) &&
-        !wf::get_core().tx_manager->is_object_pending(wtoplevel))
+    if (!wf::get_core().tx_manager->is_object_pending(wtoplevel))
     {
         // Drop self-ref => `this` might get deleted
         _self_ref.reset();

src/view/xwayland/xwayland-toplevel-view.hpp
@@ -463,8 +463,7 @@ class wayfire_xwayland_view : public wf::toplevel_view_interface_t, public wayfi
         wf::view_implementation::emit_toplevel_state_change_signals({this}, old_state);
         wf::scene::update(this->get_surface_root_node(), wf::scene::update_flag::GEOMETRY);
 
-        if (!wf::get_core().tx_manager->is_object_committed(toplevel) &&
-            !wf::get_core().tx_manager->is_object_pending(toplevel))
+        if (!wf::get_core().tx_manager->is_object_pending(toplevel))
         {
             // Drop self-reference => object might be deleted afterwards
             _self_ref.reset();