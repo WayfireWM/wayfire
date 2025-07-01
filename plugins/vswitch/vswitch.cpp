deee03822081e47f06c4b9b7a3e083c383449135
Marcus Britanicus <marcusbritanicus@gmail.com>
2025-07-01 13:12:08 +0530

    Uncrustify

--- plugins/vswitch/vswitch.cpp
+++ plugins/vswitch/vswitch.cpp
@@ -31,7 +31,7 @@ class workspace_animation_t : public duration_t
 static void move_view(wayfire_toplevel_view view, wf::point_t to_ws, bool relative = false)
 {
     // Get the wset.
-    auto wset   = view->get_wset();
+    auto wset = view->get_wset();
 
     // Coordinates for moving the view.
     // We need from, because @to_ws can be relative
@@ -45,7 +45,8 @@ static void move_view(wayfire_toplevel_view view, wf::point_t to_ws, bool relati
         wset->move_to_workspace(v, to);
     }
 
-    if ( auto output = view->get_output() ) {
+    if (auto output = view->get_output())
+    {
         wf::view_change_workspace_signal signal;
         signal.view = view;
         signal.from = from;
@@ -423,7 +424,7 @@ class vswitch : public wf::per_output_plugin_instance_t
                         return false;
                     }
 
-                    wf::vswitch::move_view( view, delta, true );
+                    wf::vswitch::move_view(view, delta, true);
                     return true;
                 }
 
@@ -639,7 +640,7 @@ class wf_vswitch_global_plugin_t : public wf::per_output_plugin_t<vswitch>
     {
         uint64_t x = wf::ipc::json_get_uint64(data, "x");
         uint64_t y = wf::ipc::json_get_uint64(data, "y");
-        uint64_t view_id   = wf::ipc::json_get_uint64(data, "view-id");
+        uint64_t view_id = wf::ipc::json_get_uint64(data, "view-id");
 
         auto view = wf::toplevel_cast(wf::ipc::find_view_by_id(view_id));
         if (!view)
@@ -663,7 +664,7 @@ class wf_vswitch_global_plugin_t : public wf::per_output_plugin_t<vswitch>
             return wf::ipc::json_error("Workspace coordinates are too big!");
         }
 
-        wf::vswitch::move_view( view, {(int)x, (int)y} );
+        wf::vswitch::move_view(view, {(int)x, (int)y});
         return wf::ipc::json_ok();
     };
 };
