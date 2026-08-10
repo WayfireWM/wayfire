okcomput3/wayfire-vertical-desktop

commit 3b6d5d5286265a58e784e18c1ece5ea867295c52

Squeezimize: horizontal version (#2732)
* squeezimize: add a horizontal version for minimizing to the left or right edge of the screen

* squeezimize: simplify some formulas

* squeezimize: improve heuristics for detecting the direction

plugins/animate/squeezimize.hpp
@@ -79,10 +79,8 @@ void main()
     float progress_pt_one = pow(clamp(progress, 0.0, 0.25) * 4.0, 2.0);
     float progress_pt_two = pow(progress, 2.0);
 
-    uv_squeeze.x = (uv.x * inv_w) - (inv_w - 1.0);
-    uv_squeeze.x += inv_w - inv_w * src_box.z;
-    uv_squeeze.y = (uv.y * inv_h) - (inv_h - 1.0);
-    uv_squeeze.y += inv_h * src_box.y;
+    uv_squeeze.x = inv_w * (uv.x - src_box.x);
+    uv_squeeze.y = inv_h * (uv.y - 1.0 + src_box.w);
 
     if (upward == 1)
     {
@@ -99,6 +97,60 @@ void main()
     uv_squeeze.x += sigmoid * progress_pt_one * (src_box.x - target_box.x) * inv_w;
     uv_squeeze.x *= (sigmoid * ((src_box.z - src_box.x) - (target_box.z - target_box.x)) /
                     (target_box.z - target_box.x) * progress_pt_one) + 1.0;
+
+    if (uv_squeeze.x < 0.0 || uv_squeeze.y < 0.0 ||
+        uv_squeeze.x > 1.0 || uv_squeeze.y > 1.0)
+    {
+        discard;
+    }
+
+    gl_FragColor = get_pixel(uv_squeeze);
+}
+)";
+
+
+static const char *squeeze_frag_source_horiz =
+    R"(
+#version 100
+@builtin_ext@
+@builtin@
+
+precision mediump float;
+
+varying highp vec2 uv;
+uniform mediump float progress;
+uniform mediump vec4 src_box;
+uniform mediump vec4 target_box;
+uniform int upward;
+
+void main()
+{
+    float y, sigmoid;
+    vec2 uv_squeeze;
+    float inv_w = 1.0 / (src_box.z - src_box.x);
+    float inv_h = 1.0 / (src_box.w - src_box.y);
+    float progress_pt_one = pow(clamp(progress, 0.0, 0.25) * 4.0, 2.0);
+    float progress_pt_two = pow(progress, 2.0);
+
+    uv_squeeze.x = inv_w * (uv.x - src_box.x);
+    uv_squeeze.y = inv_h * (1.0 - uv.y - src_box.y);
+
+    if (upward == 1)
+    {
+        y = 1.0 - uv.x;
+        uv_squeeze.x += progress_pt_two * (inv_w - target_box.z);
+        sigmoid = 1.0 / (1.0 + pow(2.718, -(y * (1.0 / (src_box.z - target_box.z)) * 15.0 - 10.0)));
+    } else
+    {
+        y = uv.x;
+        uv_squeeze.x -= progress_pt_two * (inv_w - target_box.x + target_box.z);
+        sigmoid = 1.0 / (1.0 + pow(2.718, -(y * (1.0 / (target_box.z - src_box.x)) * 15.0 - 10.0)));
+    }
+
+    uv_squeeze.y += sigmoid * progress_pt_one * (src_box.y - target_box.y) * inv_h;
+    uv_squeeze.y *= (sigmoid * ((src_box.w - src_box.y) - (target_box.w - target_box.y)) /
+                    (target_box.w - target_box.y) * progress_pt_one) + 1.0;
+    uv_squeeze.y = 1.0 - uv_squeeze.y;
 
     if (uv_squeeze.x < 0.0 || uv_squeeze.y < 0.0 ||
         uv_squeeze.x > 1.0 || uv_squeeze.y > 1.0)
@@ -276,15 +328,49 @@ class squeezimize_transformer : public wf::scene::view_2d_transformer_t
                 (this->minimize_target.y + this->minimize_target.height) - bbox.y),
                 (bbox.y + bbox.height) - this->minimize_target.y);
 
+        bool horiz;
+        // auto src_box = view->get_bounding_box();
+        auto output = view->get_output();
+        auto geom   = output->get_relative_geometry();
+
+        // check which edge the target is closest to
+        double x  = minimize_target.x + minimize_target.width / 2.0;
+        double y  = minimize_target.y + minimize_target.height / 2.0;
+        double y2 = y * geom.width / geom.height;
+        if (x < y2)
+        {
+            // bottom left part of the screen
+            if (x < geom.width - y2)
+            {
+                // left edge
+                horiz = true;
+                this->upward = true;
+            } else
+            {
+                // bottom edge
+                horiz = false;
+                this->upward = false;
+            }
+        } else
+        {
+            // top right part of the screen
+            if (x > geom.width - y2)
+            {
+                // right edge
+                horiz = true;
+                this->upward = false;
+            } else
+            {
+                // top edge
+                horiz = false;
+                this->upward = true;
+            }
+        }
+
         wf::gles::run_in_context_if_gles([&]
         {
-            program.compile(squeeze_vert_source, squeeze_frag_source);
+            program.compile(squeeze_vert_source, horiz ? squeeze_frag_source_horiz : squeeze_frag_source);
         });
-
-        auto src_box = view->get_bounding_box();
-        auto output  = view->get_output();
-        this->upward = ((src_box.y > minimize_target.y) ||
-            ((src_box.y < 0) && (minimize_target.y < output->get_relative_geometry().height / 2)));
     }
 
     wf::geometry_t get_bounding_box() override

WayfireWM/wayfire

commit 2bd9c6d175a788ba057add7eb762e8b0ca4ea7cd

squeezimize: fix default behavior
The default behavior was broken since commit `3b6d5d5286265a58e784e18c1ece5ea867295c52`, where minimize_target (the function input) was being used for calculations instead of using this->minimize_target (the member of the class that stores the final target).

plugins/animate/squeezimize.hpp
@@ -338,8 +338,8 @@ class squeezimize_transformer : public wf::scene::view_2d_transformer_t
         auto geom   = output->get_relative_geometry();
 
         // check which edge the target is closest to
-        double x  = minimize_target.x + minimize_target.width / 2.0;
-        double y  = minimize_target.y + minimize_target.height / 2.0;
+        double x  = this->minimize_target.x + this->minimize_target.width / 2.0;
+        double y  = this->minimize_target.y + this->minimize_target.height / 2.0;
         double y2 = y * geom.width / geom.height;
         if (x < y2)
         {