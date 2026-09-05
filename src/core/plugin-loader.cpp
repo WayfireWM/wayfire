f2e97a233f395e349aa3f3386da556a9cd903613
Kondor Dániel <kondor.dani@gmail.com>
2026-09-05 12:55:29 +0200

    plugin-loader: also include legacy plugin paths for compatibility

--- src/core/plugin-loader.cpp
+++ src/core/plugin-loader.cpp
@@ -327,8 +327,22 @@ std::vector<std::string> wf::get_plugin_paths()
     if (xdg_data_dir != "")
     {
         // REL_LIBDIR from meson.build: get_option('libdir') to be compatible with wayfire-plugin
+        std::string rel_libdir = REL_LIBDIR;
         plugin_prefixes.push_back(xdg_data_dir + "/wayfire/plugin-manager/install/" +
-            REL_LIBDIR + "/wayfire");
+            rel_libdir + "/wayfire");
+
+        // previous hardcoded paths for wayfire-plugin
+        if (rel_libdir != "lib")
+        {
+            plugin_prefixes.push_back(xdg_data_dir + "/wayfire/plugin-manager/install/lib/wayfire");
+        }
+
+        if (rel_libdir != "lib64")
+        {
+            plugin_prefixes.push_back(xdg_data_dir + "/wayfire/plugin-manager/install/lib64/wayfire");
+        }
+
+        // additional path for manually installed plugins
         plugin_prefixes.push_back(xdg_data_dir + "/wayfire/plugins");
     }
 
